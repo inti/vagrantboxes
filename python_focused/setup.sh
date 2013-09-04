3c380e945774bfcb3f68cb36a284660176330dfe
inti <intipedroso@gmail.com>
2013-09-04 09:29:39 +0100

    minor change

---
+++ python_focused/setup.sh
@@ -0,0 +1,16 @@
+vagrant box add precise64 http://files.vagrantup.com/precise64.box
+vagrant init precise64
+vagrant up
+
+# requirments
+gem install berkshelf
+
+# 
+mkdir cookbooks
+# into cookbook
+cd cookbooks/
+git clone https://github.com/opscode-cookbooks/apt.git
+git clone https://github.com/opscode-cookbooks/python.git
+git clone https://github.com/opscode-cookbooks/git.git
+#git clone https://github.com/rgbkrk/ipynb-cookbook.git ipython
+git clone https://github.com/bsweger/python-data.git
